a17ccba69702b2e95c1c0c1933f552e22c210938
YooBao <164113940+kyy98@users.noreply.github.com>
2024-05-18 16:25:05 +0900

    [Bronze II] Title: 소수 찾기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.py
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.py
@@ -1,10 +1,23 @@
-lst_1=[]
-a = int(input())
-N = map(int, input().split())
-M = list(N)
-for i in M:
-    lst_2 = [k for k in range(1, i+1) if i % k == 0]
-    lst_1.append(lst_2)
+import sys
 
-lst_3 = [M[j] for j in range(len(M)) if len(lst_1[j])==2]
-print(len(lst_3))
+N = int(sys.stdin.readline())
+
+li = list(map(int, sys.stdin.readline().split()))
+
+cnt = 0
+for i in li:
+    if i == 1:
+        continue
+    elif i <= 3:
+        cnt += 1
+    else:
+        n = 0
+        for j in range(2, i):
+            if i % j == 0:
+                n += 1
+                break
+        if n == 0:
+            cnt += 1
+
+print(cnt)
+
