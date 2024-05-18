d43295877de24ccf2f67cc84be620e001908b960
YooBao <164113940+kyy98@users.noreply.github.com>
2024-05-18 16:06:55 +0900

    [Bronze II] Title: 블랙잭, Time: 84 ms, Memory: 37492 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.py
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -1,17 +1,13 @@
-a, M = map(int, input().split())
-
-lst = list(map(int, input().split()))
+N, M = map(int, input().split())
 
 lst_2 = []
-for i in range(len(lst)-1):
-    for j in range(i+1, len(lst)):
-        for k in range(j+1, len(lst)):
-            sum = lst[i]+lst[j]+lst[k]
-            if sum > M:
-                continue
-            else:
-                lst_2.append(sum)
-            
+lst = list(map(int, input().split()))
 
+for i in range(N):
+    for j in range(i+1, N):
+        for k in range(j+1, N):
+            sum = lst[i]+lst[j]+lst[k]
+            if sum <= M:
+                lst_2.append(sum)
 
 print(max(lst_2))
