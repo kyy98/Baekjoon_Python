62f502097ed6600084154f304851d1e29f16cdda
YooBao <164113940+kyy98@users.noreply.github.com>
2024-05-19 22:28:39 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 9468 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
@@ -0,0 +1,12 @@
+import sys
+N = int(input())
+count = [0]*10001
+
+for i in range(N):
+    input_num = int(sys.stdin.readline())
+    count[input_num]+=1
+
+    
+for i in range(len(count)):
+    for j in range(count[i]):
+        print(i)
