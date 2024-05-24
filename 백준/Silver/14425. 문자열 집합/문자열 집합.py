c6624e4ade59c0c97c0413508244f46a7a63951e
YooBao <164113940+kyy98@users.noreply.github.com>
2024-05-24 23:03:49 +0900

    [Silver IV] Title: 문자열 집합, Time: 920 ms, Memory: 42444 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.py
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.py
@@ -5,13 +5,14 @@ cnt = 0
 for i in range(N):
     w = input()
     lst_1.append(w)
+set_1 = set(lst_1)
 
 for j in range(M):
     a = input()
     lst_2.append(a)
 
 for k in lst_2:
-    if k in lst_1:
+    if k in set_1:
         cnt+=1
 
 print(cnt)
