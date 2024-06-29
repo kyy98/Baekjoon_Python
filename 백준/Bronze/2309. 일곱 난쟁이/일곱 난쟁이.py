4f62afb92e511491d495f9035d08f1d15971a008
YooBao <164113940+kyy98@users.noreply.github.com>
2024-06-29 16:40:49 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
@@ -1,15 +1,15 @@
-li = []
+lst = []
 for i in range(9):
-    row = int(input())
-    li.append(row)
+    height = int(input())
+    lst.append(height)
 
-for i in li:
-    for j in li:
-        res = sum(li) - i - j
-        if (res == 100) and (i!=j):
-            li.remove(i)
-            li.remove(j)
-li.sort()
+for i in range(len(lst)-1):
+    for j in range(i+1, len(lst)):
+        s = lst[i]+lst[j]
+        if 100 == sum(lst)-s:
+            a,b = lst[i], lst[j]
+lst.remove(a)
+lst.remove(b)
 
-for i in li:
+for i in sorted(lst):
     print(i)
