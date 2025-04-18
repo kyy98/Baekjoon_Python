1e0cfb9ecbc97f442519bfe9d60009c054927edd
YooBao <164113940+kyy98@users.noreply.github.com>
2025-04-19 00:20:03 +0900

    [level 2] Title: 기능개발, Time: 0.04 ms, Memory: 9.13 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,22 +1,45 @@
-import math
+# import math
+# progresses = [93, 30, 55]
+# speeds = [1, 30, 5]
+
+# def solution(progresses, speeds):
+#     ls = [math.ceil((100-progresses[i])/speeds[i]) for i in range(len(progresses))]
+    
+#     answer = []
+#     idx = 0
+#     for i in range(len(ls)):
+#         if ls[idx] < ls[i]:
+#             answer.append(i-idx)
+#             idx = i
+#     answer.append(len(ls)-sum(answer))
+#     return answer
+
+# solution(progresses, speeds)
+            
+        
 progresses = [93, 30, 55]
 speeds = [1, 30, 5]
 
 def solution(progresses, speeds):
-    ls = [math.ceil((100-progresses[i])/speeds[i]) for i in range(len(progresses))]
-    
     answer = []
-    idx = 0
-    for i in range(len(ls)):
-        if ls[idx] < ls[i]:
-            answer.append(i-idx)
-            idx = i
-    answer.append(len(ls)-sum(answer))
-    return answer
-
-solution(progresses, speeds)
+    time = 0
+    cnt = 0
+    while len(progresses)>0:
+        if progresses[0] + time*speeds[0] >= 100:
+            cnt+=1
+            progresses.pop(0)
+            speeds.pop(0)  
             
-        
+        else:
+            if cnt > 0:
+                answer.append(cnt)
+                cnt = 0
+                time+=1
+            else:
+                time+=1
+    answer.append(cnt)
+    return answer
+solution(progresses, speeds)
             
             
 
