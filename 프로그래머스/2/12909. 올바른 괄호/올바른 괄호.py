867e560afa472d129949885aa588530c4c72d335
YooBao <164113940+kyy98@users.noreply.github.com>
2025-04-20 11:30:58 +0900

    [level 2] Title: 올바른 괄호, Time: 6.73 ms, Memory: 9.48 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -0,0 +1,27 @@
+s = '()()'
+
+# def solution(s):
+#     ls = []
+#     for i in s:
+#         if i == '(':
+#             ls.append(i)
+#         else:
+#             if ls == []:
+#                 return False
+#             else:
+#                 ls.pop(0)
+#     if ls != []:
+#         return False
+#     return True
+#     solution(s)
+
+def solution(s):
+    stack = []
+    for i in s:
+        if i == '(':
+            stack.append(i)
+        else:
+            if not stack:
+                return False
+            stack.pop()
+    return not stack
