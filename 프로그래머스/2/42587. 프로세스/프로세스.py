62bd8248ca67b529666569a56032e76a22e488be
YooBao <164113940+kyy98@users.noreply.github.com>
2025-04-20 13:32:47 +0900

    [level 2] Title: 프로세스, Time: 0.72 ms, Memory: 9.11 MB -BaekjoonHub

---
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -0,0 +1,18 @@
+priorities = [2, 1, 3, 2]
+location = 2
+
+from collections import deque
+
+def solution(priorities, location):
+    cnt = 0
+    queue = deque([(v, i) for i, v in enumerate(priorities)])
+    
+    while queue:
+        current = queue.popleft()
+        if any(current[0] < i[0] for i in queue):
+            queue.append(current)
+        else:
+            cnt+=1
+            if current[1] == location:
+                return cnt
+solution(priorities, location)
