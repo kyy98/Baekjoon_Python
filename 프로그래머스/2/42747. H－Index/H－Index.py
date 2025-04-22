7c8d44799622ec6422a399dac7c6a7861254fb6a
YooBao <164113940+kyy98@users.noreply.github.com>
2025-04-22 16:06:58 +0900

    [level 2] Title: H-Index, Time: 0.19 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -1,26 +1,14 @@
 citations = [3, 0, 6, 1, 5]
 
-# def solution(citations):
-#     h_idx = 0
-#     cit = sorted(citations, reverse=True)
-#     for i, j in enumerate(cit):
-#         if j <= i+1:
-#             return j
-#     return len(citations)-1
-            
-    
-# solution(citations)            
-            
-1
-2
-3
-4
-5
 def solution(citations):
-    citations.sort(reverse=True)
-    answer = max(map(min, enumerate(citations, start=1)))
-    return answer
-
+    cit = sorted(citations, reverse=True)
+    for i in range(len(cit)):
+        if cit[i] < i+1:
+            return i
+            
+    return len(citations)
+solution(citations)            
+            
 
 
             
