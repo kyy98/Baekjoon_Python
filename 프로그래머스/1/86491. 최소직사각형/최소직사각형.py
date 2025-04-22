4dbdb717fba427e99761a33bf9ee8ebddfd844ae
YooBao <164113940+kyy98@users.noreply.github.com>
2025-04-22 18:10:17 +0900

    [level 1] Title: 최소직사각형, Time: 2.45 ms, Memory: 10.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
@@ -0,0 +1,18 @@
+sizes = [[60, 50], [30, 70], [60, 30], [80, 40]]
+
+def solution(sizes):
+    ls_1 = []
+    ls_2 = []
+    for i in sizes:
+        if i[0] < i[1]:
+            temp = i[0]
+            i[0] = i[1]
+            i[1] = temp
+    for i in sizes:
+        ls_1.append(i[0])
+        ls_2.append(i[1])
+        
+    return max(ls_1)*max(ls_2)
+solution(sizes)
+        
+            
