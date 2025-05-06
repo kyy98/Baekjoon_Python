e3e447baab6a21d04c9aee6d735ed87b66b3e693
YooBao <164113940+kyy98@users.noreply.github.com>
2025-05-07 08:33:28 +0900

    [level 1] Title: 체육복, Time: 0.02 ms, Memory: 9.32 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.py
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -1,45 +1,18 @@
-# def solution(n, lost, reserve):
-#     lost = set(lost) - set(reserve)
-#     reserve = set(reserve) - set(lost)
-
-#     for r in sorted(reserve):
-#         if r - 1 in lost:
-#             lost.remove(r - 1)
-#         elif r + 1 in lost:
-#             lost.remove(r + 1)
-
-#     return n - len(lost)
 def solution(n, lost, reserve):
-    answer = 0
-    lost = sorted(lost)
-    total = [1] * (n+2)
-    total[0] = 0
-    total[-1] = 0
-    
-    for i in lost:
-        total[i] -= 1
-        
-    for i in reserve:
-        total[i] += 1
-    
-    for i in lost:
-        if total[i] == 0 and total[i-1] == 2:
-            total[i-1] = 1
-            total[i] = 1
-        if total[i] == 0 and total[i+1]==2:
-            total[i+1] = 1
-            total[i] = 1
-            
-    print(total) # 확인용
-    
-    for i in total:
-        if i > 0:
-            answer += 1
-    
-    return answer
+    # 정렬
+    lost.sort()
+	
+    # 여벌 체육복을 가져온 학생도 도난당했을 수 있기 때문에 실질적으로 빌려줄 수 있는 학생만 걸러줌
+    r = [i for i in reserve if i not in lost]
+    l = [j for j in lost if j not in reserve]
+    r = sorted(r)
+    l = sorted(l)
+	
+    # 체육복 빌려주기(나의 앞 번호부터 확인)
+    for i in r:
+        if i-1 in l:
+            l.remove(i-1)
+        elif i+1 in l:
+            l.remove(i+1)
 
-# 연속된 번호의 학생에게만 빌려줄수있음
-# 최대한 많은 학생이 체육복을 가지고있어야함
-# 앞으로 줄지 뒤로줄지 고민해야함
-
-# 뒷번호에게 줄때, 그 뒷번호의뒤에 2인사람이 없으면 그냥 주기
+    return n-len(l)
