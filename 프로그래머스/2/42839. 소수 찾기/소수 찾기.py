2d474ee58eca16cb254332c37d8c95a533e1352c
YooBao <164113940+kyy98@users.noreply.github.com>
2025-05-07 15:02:46 +0900

    [level 2] Title: 소수 찾기, Time: 7.36 ms, Memory: 10.7 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -1,3 +1,26 @@
+# from itertools import permutations
+# import math
+
+# def sosu(n):
+#     if n < 2:
+#         return False
+#     else:
+#         for i in range(2, int(math.sqrt(n)+1)):
+#             if n % i == 0:
+#                 return False
+#         return True
+
+# def solution(numbers):
+#     cnt = 0
+#     p = []
+#     for i in range(1, len(numbers)+1):
+#         p.extend(permutations(numbers, i))
+#     perm = list(int(''.join(k)) for k in p)
+#     for i in set(perm):
+#         if sosu(i):
+#             cnt+=1
+#     return cnt
+            
 from itertools import permutations
 import math
 
@@ -10,17 +33,33 @@ def sosu(n):
                 return False
         return True
 
+
 def solution(numbers):
-    cnt = 0
-    p = []
+    ls = []
     for i in range(1, len(numbers)+1):
-        p.extend(permutations(numbers, i))
-    perm = list(int(''.join(k)) for k in p)
-    for i in set(perm):
+        ls.extend(permutations(numbers, i))    # permutations 결과 list에 넣어줘야 함
+    
+    for i in range(len(ls)):
+        ls[i] = int(''.join(ls[i]))
+    
+    
+    cnt = 0
+    ls = list(set(ls))
+    for i in ls:
         if sosu(i):
             cnt+=1
     return cnt
-            
-
+        
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
         
