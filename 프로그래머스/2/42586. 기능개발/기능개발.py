88a9fa5533da1cc44ffe0fe9ca356d5eed2be33e
YooBao <164113940+kyy98@users.noreply.github.com>
2025-10-25 12:49:31 +0900

    [level 2] Title: 기능개발, Time: 0.04 ms, Memory: 9.24 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,43 +1,25 @@
+from collections import deque
 import math
-# progresses = [93, 30, 55]
-# speeds = [1, 30, 5]
 
 def solution(progresses, speeds):
-    ls = [math.ceil((100-progresses[i])/speeds[i]) for i in range(len(progresses))]
-    
     answer = []
-    idx = 0
-    for i in range(len(ls)):
-        if ls[idx] < ls[i]:
-            answer.append(i-idx)
-            idx = i
-    answer.append(len(ls)-sum(answer))
+    dq = deque([])
+    for i in range(len(progresses)):
+        day = math.ceil((100-progresses[i])/speeds[i])
+        dq.append(day)
+    
+    while dq:
+        cnt = 1
+        d1 = dq.popleft()
+        
+        while dq:
+            d2 = dq.popleft()
+            if d1 >= d2:
+                cnt+=1
+            else:
+                dq.appendleft(d2)
+                break
+        answer.append(cnt)
+
+                       
     return answer
-
-# solution(progresses, speeds)
-        
-
-# def solution(progresses, speeds):
-#     answer = []
-#     time = 0
-#     cnt = 0
-#     while len(progresses)>0:
-#         if progresses[0] + time*speeds[0] >= 100:
-#             cnt+=1
-#             progresses.pop(0)
-#             speeds.pop(0)  
-            
-#         else:
-#             if cnt > 0:
-#                 answer.append(cnt)
-#                 cnt = 0
-#                 time+=1
-#             else:
-#                 time+=1
-#     answer.append(cnt)
-#     return answer
-
-            
-            
-
-        
