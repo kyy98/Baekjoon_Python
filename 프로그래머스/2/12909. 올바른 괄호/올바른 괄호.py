5e4fe4a78a3fe67b327cf9b2bdb6ea3cac0a17fd
YooBao <164113940+kyy98@users.noreply.github.com>
2025-10-25 16:04:29 +0900

    [level 2] Title: 올바른 괄호, Time: 9.76 ms, Memory: 9.51 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,14 +1,20 @@
-
-
 def solution(s):
     ls = []
-    for i in s:
-        if i == '(':
-            ls.append(i)
-        else:
-            if ls == []:     # 스택이 비어 있으면 닫는 괄호가 많다는 뜻
-                return False
-            ls.pop()         # 마지막 여는 괄호 제거 (pop(0) ❌ pop() ✅)
-    if ls != []:             # 여는 괄호가 남아 있음 → 짝이 안 맞음
+    if s[0] == ')':
         return False
-    return True
+    else:
+        try:
+            for i in range(len(s)):
+                if s[i] == '(':
+                    ls.append(s[i])
+                else:
+                    ls.pop()
+            if len(ls) > 0:
+                return False
+
+            else:
+                return True
+        except IndexError:
+            return False
+            
+        
