f23365c1f45cdbb6579a345dd9714b3b4057ba17
YooBao <164113940+kyy98@users.noreply.github.com>
2025-10-25 19:44:16 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 39.08 ms, Memory: 26.9 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,27 +1,22 @@
-participant = ["leo", "kiki", "eden"]
-completion = ["eden", "kiki"]
-
-# def solution(participant, completion):
-    # dict_1 = dict(zip(participant, [participant.count(i) for i in participant]))
-
-#     for i in participant:
-#         if i not in completion:
-#             return i
-#         else:
-#             if participant.count(i) != completion.count(i):
-#                 return i
-            
-
-# solution(participant, completion)
-
+from collections import Counter
 def solution(participant, completion):
-    hash_dict = dict()
-    p_sum = 0
-    for p in participant:
-        hash_dict[hash(p)] = p
-        p_sum+=hash(p)
-    for c in completion:
-        p_sum-=hash(c)
-    return hash_dict[p_sum]
-solution(participant, completion)     
+    dict_1 = Counter(participant)
+    dict_2 = Counter(completion)
+#     dict_1 = {} 
+#     dict_2 = {}
+#     for i in range(len(participant)):
+#         name_p = participant[i]
+#         count_p = participant.count(name_p)
+#         dict_1[name_p] = count_p  
+#     for j in range(len(completion)):
+#         name_c = completion[j]
+#         count_c = completion.count(name_c)
+#         dict_2[name_c] = count_c
+  
+    for k in dict_1.keys():
+        if k not in dict_2.keys():
+            return k
+        else:
+            if dict_2[k] != dict_1[k]:
+                return k
     
