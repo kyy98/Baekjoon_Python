fb052b1dd4068ff6429a81ca63b0a1cd3bc39700
YooBao <164113940+kyy98@users.noreply.github.com>
2025-10-25 20:07:14 +0900

    [level 1] Title: 폰켓몬, Time: 1.16 ms, Memory: 9.79 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -1,11 +1,10 @@
-nums = [3,1,2,3]
 def solution(nums):
-    # ls = list(set(nums))
-    # return min(len(nums)/2, len(ls))
+    dict_n = dict()
     
-    ls = list(set(nums))
-    if len(ls) >= len(nums)/2:
-        return len(nums)/2
+    for n in nums:
+        dict_n[n] = 1
+    if len(dict_n) < len(nums)//2:
+        return len(dict_n)
     else:
-        return len(ls)
-solution(nums)
+        return len(nums)//2
+    
