a2bc4d257c35cfbf2cfc086e00cdab4489a65893
YooBao <164113940+kyy98@users.noreply.github.com>
2025-10-25 17:50:13 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 9.27 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,13 +1,8 @@
-commands = [[2, 5, 3], [4, 4, 1], [1, 7, 3]]
-array = [1,5,2,6,3,7,4]
-
 def solution(array, commands):
-    ls = []
-    for a in commands:
-        arr1 = array[a[0]-1:a[1]]
-        arr1 = sorted(arr1)
-        b = arr1[a[2]-1]
-        ls.append(b)
-    return ls
-solution(array, commands)
-    
+    answer = []
+    for c in commands:
+        array_2 = array[c[0]-1:c[1]]
+        array_3 = sorted(array_2)
+        idx = array_3[c[2]-1]
+        answer.append(idx)
+    return answer
