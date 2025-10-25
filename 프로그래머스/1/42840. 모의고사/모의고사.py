330c845442660b9ed3d1e963baed24de8f0ad6f8
YooBao <164113940+kyy98@users.noreply.github.com>
2025-10-25 23:08:00 +0900

    [level 1] Title: 모의고사, Time: 3.65 ms, Memory: 9.43 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,59 +1,19 @@
-# st_1 = [1,2,3,4,5]
-# st_2 = [2,1,2,3,2,4,2,5]
-# st_3 = [3,3,1,1,2,2,4,4,5,5]
-
-# score = [0, 0, 0]
-# answers = [1,2,3,4,5]
-
-# def solution(answers):
-#     for i in range(len(answers)):
-#         if answers[i] == st_1[i%len(st_1)]:
-#             score[0]+=1
-#         if answers[i] == st_2[i%len(st_2)]:
-#             score[1]+=1
-#         if answers[i] == st_3[i%len(st_3)]:
-#             score[2]+=1
-
-#     max_score = max(score)
-#     result = []
-#     for i in range(3):
-#         if score[i] == max_score:
-#             result.append(i + 1)  # 수포자는 1번부터 시작
-#     return result
-s1 = [1,2,3,4,5]
-s2 = [2,1,2,3,2,4,2,5]
-s3 = [3,3,1,1,2,2,4,4,5,5]
-ls = [0,0,0]
-
 def solution(answers):
+    s1 = [1,2,3,4,5]
+    s2 = [2,1,2,3,2,4,2,5]
+    s3 = [3,3,1,1,2,2,4,4,5,5]
+    
+    ls = [0,0,0]
     for i in range(len(answers)):
-        if answers[i] == s1[i%len(s1)]:
+        if answers[i] == s1[i%(len(s1))]:
             ls[0]+=1
-        if answers[i] == s2[i%len(s2)]:
+        if answers[i] == s2[i%(len(s2))]:
             ls[1]+=1
-        if answers[i] == s3[i%len(s3)]:
+        if answers[i] == s3[i%(len(s3))]:
             ls[2]+=1
-    max_score = max(ls)
-    ans = []
-    for i in range(3):
-        if max_score == ls[i]:
-            ans.append(i+1)
-    return ans
-            
-            
-            
+    ls_2 = []
+    for num in enumerate(ls):
+        if num[1] == max(ls):
+            ls_2.append(num[0]+1)
+    return ls_2
     
-    
-        
-
-
-
-
-
-
-
-
-
-
-
-
