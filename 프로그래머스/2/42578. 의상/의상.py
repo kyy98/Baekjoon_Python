9b5eb7443f9008d3583bf468bc6096bb771b5146
YooBao <164113940+kyy98@users.noreply.github.com>
2025-10-26 20:10:59 +0900

    [level 2] Title: 의상, Time: 0.02 ms, Memory: 9.26 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,27 +1,13 @@
-clothes = [["yellow_hat", "headgear"], ["blue_sunglasses", "eyewear"], ["green_turban", "headgear"]]
-
-
 def solution(clothes):
-    types = []
-    answer = 1
-    for i in clothes:
-        types.append(i[1])
-        types = list(set(types))
-
-    hash_map = {t: 0 for t in types}
+    dict = {}
+    num = 1
+    for c in clothes:
+        if c[1] not in dict:
+            dict[c[1]] = [c[0]]
+        else:
+            dict[c[1]] += [c[0]]
+    for v in dict.values():
+        num *= len(v)+1
+    return num-1
+        
     
-    for n, t in clothes:
-        hash_map[t] += 1
-        
-        
-    if len(hash_map) == 1:
-        return next(iter(hash_map.values()))
-    else:
-        for v in hash_map.values():
-            answer *= (v+1)
-        return answer-1
-solution(clothes)
-
-        
-        
-        
