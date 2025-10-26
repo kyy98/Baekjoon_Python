b0e08e84b105daf55710b4aa950306af8e3552cd
YooBao <164113940+kyy98@users.noreply.github.com>
2025-10-26 11:29:23 +0900

    [level 2] Title: 카펫, Time: 0.39 ms, Memory: 9.19 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -1,14 +1,15 @@
 def solution(brown, yellow):
+    answer = []
     yellow_x = 0
     yellow_y = 0
-    answer = []
     for i in range(1, yellow+1):
-        if yellow%i == 0:
-            yellow_x = yellow//i
+        if yellow % i == 0:
+            yellow_x = yellow // i
             yellow_y = i
-            if 2*yellow_x+2*yellow_y+4 == brown+yellow-(yellow_x*yellow_y):
+            if 2*(yellow_x)+2*(yellow_y)+4 == brown:
                 answer.append(yellow_x+2)
                 answer.append(yellow_y+2)
                 break
+        answer = sorted(answer)
     return answer
-    
+            
