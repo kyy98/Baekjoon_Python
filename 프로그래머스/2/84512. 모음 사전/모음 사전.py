a75ab6cb980539833607f7609f7c651c8d5ec4b7
YooBao <164113940+kyy98@users.noreply.github.com>
2025-10-26 14:04:57 +0900

    [level 2] Title: 모음 사전, Time: 2.00 ms, Memory: 9.36 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.py
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -1,39 +1,10 @@
 from itertools import product
-
-# def solution(word):
-#     result = []
-#     for i in range(1, 6):
-#         ls = list(product(["A", "E", "I", "O", "U"], repeat=i))
-#         for j in ls:
-#             result.append(''.join(j))
-#     result = sorted(result)
-#     return result.index(word)+1
-            
-            
 def solution(word):
+    moeum = ['A', 'E', 'I', 'O', 'U']
     ls = []
-    for i in range(1,6):
-        m = ['A', 'E', 'I', 'O', 'U']
-        p = list(product(m, repeat = i))
-        for i in p:
-            ls.append(i)
-    num = [''.join(j) for j in ls]
-    return sorted(num).index(word)+1
-        
-
-        
-    
-        
-    
-        
-        
-        
-        
-        
-        
-        
-        
-        
-## itertools.product(1~5)
-## 그리고 sort
-## idx+1
+    for i in range(1, 6):
+        for p in product(moeum, repeat=i):
+            w = ''.join(p)
+            ls.append(w)
+    ls = sorted(ls)
+    return ls.index(word)+1
