8b3493951af3bb180e29c924c7b9a1f0d22bfc95
YooBao <164113940+kyy98@users.noreply.github.com>
2025-10-26 13:26:08 +0900

    [level 2] Title: 피로도, Time: 69.18 ms, Memory: 11.7 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -1,42 +1,19 @@
 from itertools import permutations
-
-# def solution(k, dungeons):
-    # max_cnt = 0
-    # for perm in permutations(dungeons, len(dungeons)):
-    #     cnt = 0
-    #     cur_k = k
-    #     for min, consume in perm:
-    #         if cur_k >= min:
-    #             cur_k-=consume
-    #             cnt+=1
-    #         else:
-    #             break
-    #     max_cnt = max(max_cnt, cnt)
-    # return max_cnt
-     
-        
-        
-from itertools import permutations
 def solution(k, dungeons):
-    p = list(permutations(dungeons, len(dungeons)))
     ls = []
-    for d in p:
-        cur_k = k
+    for p in permutations(dungeons, len(dungeons)):
         cnt = 0
-        for i in range(len(d)):
-            if cur_k >= d[i][0]:
-                cur_k-=d[i][1]
+        temp_k = k
+        for i in range(len(dungeons)):
+            if temp_k >= p[i][0]:
                 cnt+=1
+                temp_k-=p[i][1]
             ls.append(cnt)
-    max_score = max(ls)
-    return max_score
-            
-    
-    
-
-              
-
-            
                 
+            
+    return max(ls)
     
-                
+            
+            
+        
+        
