f44bd2e6e30849b8d74bd6e4124d2f8e68a0e0ee
YooBao <164113940+kyy98@users.noreply.github.com>
2025-10-28 20:12:26 +0900

    [level 3] Title: 네트워크, Time: 0.84 ms, Memory: 9.27 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,21 +1,26 @@
 def solution(n, computers):
     def bfs(s):
         q = []
+        
         q.append(s)
         v[s] = 1
         
         while q:
-            c = q.pop(0)
+            k = q.pop(0)
+            
             for i in range(n):
-                if not v[i] and computers[c][i] == 1:
+                if computers[k][i] == 1 and v[i] == 0:
                     q.append(i)
                     v[i] = 1
-                    
     v = [0]*n
     cnt = 0
     for i in range(n):
-        if not v[i]:
+        if v[i] == 0:
             bfs(i)
             cnt+=1
     return cnt
-                    
+        
+        
+               
+        
+    
