86ed425a3ec3ac40106a8d67dbce60e49164e69f
YooBao <164113940+kyy98@users.noreply.github.com>
2025-10-28 14:32:43 +0900

    [level 2] Title: 숫자의 표현, Time: 6.12 ms, Memory: 9.11 MB -BaekjoonHub

---
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
@@ -0,0 +1,16 @@
+def solution(n):
+    cnt = 0
+    for i in range(1, n+1):
+        s = 0
+        for j in range(i, n+1):
+            s+=j
+            if s == n:
+                cnt+=1
+                break
+            if s > n:
+                break
+            
+            
+    return cnt
+                
+            
