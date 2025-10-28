b28d2514fb71c5e3c511321c30a6c52cd1951e4c
YooBao <164113940+kyy98@users.noreply.github.com>
2025-10-28 19:04:50 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 11.33 ms, Memory: 9.27 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,3 +1,39 @@
+def solution(maps):
+    N = len(maps)
+    M = len(maps[0])
+    ls = []
+    
+    def bfs(si, sj, N, M):
+        q = []
+        v = [[0]*M for _ in range(N)]
+        cnt = 0
+        
+        q.append((si,sj))
+        v[si][sj] = 1
+        
+        while q:
+            ci, cj = q.popleft()
+            for di, dj in ((-1,0),(0,-1), (0,1), (1,0)):
+                ni, nj = ci+di, cj+dj
+                if 0<=ni<N and 0<=nj<M and maps[ni][nj]==1 and v[ni][nj]==0:
+                    q.append((ni,nj))
+                    v[ni][nj] = 1
+                    cnt+=1
+        ls.append(cnt)
+        if ls:
+            return min(ls)
+        else:
+            return -1
+
+
+
+
+
+
+
+
+
+
 
 # def solution(maps):
 #     n = len(maps)
