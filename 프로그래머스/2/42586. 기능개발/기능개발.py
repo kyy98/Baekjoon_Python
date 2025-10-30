2e03f017ba786de11c483b3bfad5ac40ff7415a6
YooBao <164113940+kyy98@users.noreply.github.com>
2025-10-30 20:03:29 +0900

    [level 2] Title: 기능개발, Time: 0.04 ms, Memory: 9.18 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -4,22 +4,21 @@ import math
 def solution(progresses, speeds):
     answer = []
     dq = deque([])
+    
     for i in range(len(progresses)):
         day = math.ceil((100-progresses[i])/speeds[i])
         dq.append(day)
-    
+        
     while dq:
         cnt = 1
         d1 = dq.popleft()
-        
         while dq:
             d2 = dq.popleft()
-            if d1 >= d2:
+            if d1>=d2:
                 cnt+=1
             else:
                 dq.appendleft(d2)
                 break
+                
         answer.append(cnt)
-
-                       
     return answer
