725aba449b5c150232000ece2892254d6edaf4ce
YooBao <164113940+kyy98@users.noreply.github.com>
2025-11-02 21:07:54 +0900

    [level 2] Title: 소수 찾기, Time: 8.43 ms, Memory: 9.46 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -1,27 +1,17 @@
 from itertools import permutations
 
-ls_1 = []
-ls_2 = []
 def solution(numbers):
-    for n in numbers:
-        ls_1.append(n)
-    for i in range(1, len(ls_1)+1):
-        for p in permutations(ls_1, i):
-            num = ''.join(p)
-            ls_2.append(num)
-    ls_3 = list(set(ls_2))
-    ls_3 = [num for num in ls_3 if num[0] != '0' and num != '1']
+    nums = list(numbers)
+    all_nums = set()
     
-    sosu = []
-
-    for num in ls_3:
-        if all(int(num) % i != 0 for i in range(2, int(num))):
-            sosu.append(num)
-                  
-    return len(sosu)
+    for i in range(1, len(nums)+1):
+        for p in permutations(nums, i):
+            all_nums.add(int(''.join(p)))
             
-            
-            
-            
+    primes = []
+    for n in all_nums:
+        if n>1 and all(n%i!=0 for i in range(2, int(n**0.5)+1)):
+            primes.append(n)
+    return len(primes)
         
     
