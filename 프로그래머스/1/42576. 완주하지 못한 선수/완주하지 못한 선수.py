2cbdd28eff6fd0e1d0592d88ff84ca78d0810348
YooBao <164113940+kyy98@users.noreply.github.com>
2025-11-02 20:31:01 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 37.09 ms, Memory: 38 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,22 +1,32 @@
 from collections import Counter
+
 def solution(participant, completion):
     dict_1 = Counter(participant)
     dict_2 = Counter(completion)
-#     dict_1 = {} 
-#     dict_2 = {}
-#     for i in range(len(participant)):
-#         name_p = participant[i]
-#         count_p = participant.count(name_p)
-#         dict_1[name_p] = count_p  
-#     for j in range(len(completion)):
-#         name_c = completion[j]
-#         count_c = completion.count(name_c)
-#         dict_2[name_c] = count_c
-  
+    
     for k in dict_1.keys():
         if k not in dict_2.keys():
             return k
         else:
             if dict_2[k] != dict_1[k]:
                 return k
+#     dict_1 = Counter(participant)
+#     dict_2 = Counter(completion)
+# #     dict_1 = {} 
+# #     dict_2 = {}
+# #     for i in range(len(participant)):
+# #         name_p = participant[i]
+# #         count_p = participant.count(name_p)
+# #         dict_1[name_p] = count_p  
+# #     for j in range(len(completion)):
+# #         name_c = completion[j]
+# #         count_c = completion.count(name_c)
+# #         dict_2[name_c] = count_c
+  
+#     for k in dict_1.keys():
+#         if k not in dict_2.keys():
+#             return k
+#         else:
+#             if dict_2[k] != dict_1[k]:
+#                 return k
     
