6b243e7acbcf4998b839f9c50fb12f5ce7be6fbb
YooBao <164113940+kyy98@users.noreply.github.com>
2025-11-02 18:01:59 +0900

    [level 2] Title: 프로세스, Time: 0.95 ms, Memory: 9.24 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.py
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -1,7 +1,7 @@
 from collections import deque
 def solution(priorities, location):
-    answer = [] # 실행된 프로세스
-    dq = deque((i, j) for i, j in enumerate(priorities))
+    answer = []
+    dq = deque((i,j) for i,j in enumerate(priorities))
     while dq:
         process = dq.popleft()
         if any(process[1] < q[1] for q in dq):
@@ -11,7 +11,3 @@ def solution(priorities, location):
     for d in answer:
         if d[0] == location:
             return answer.index(d)+1
-            
-            
-        
-        
