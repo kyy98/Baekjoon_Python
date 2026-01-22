6490c61e7be2a121f1644bd12a1fa5b36ad475bb
YooBao <164113940+kyy98@users.noreply.github.com>
2026-01-22 11:55:53 +0900

    [level 2] Title: 큰 수 만들기, Time: 85.91 ms, Memory: 11.9 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
@@ -1,24 +1,30 @@
-## 맨 앞자리수가 클수록 숫자는 커진다는 점을 이용
-## 스택을 이용해서 맨 앞자리부터 순차적으로 수를 하나씩 검사하며, 
-## 앞자리 수를 가능한 크게 만드는 선에서 k개의 숫자를 제외
 def solution(number, k):
     stack = []
+    
     for num in number:
-        while len(stack) > 0 and k > 0 and num > stack[-1]:
+        while stack and stack[-1] < num and k>0:
             stack.pop()
             k-=1
         stack.append(num)
-    if k:
-        return number[:k+1]
-    else:
-        return ''.join(i for i in stack)
-        
-
     
-    
+    if k!=0:
+        stack = stack[:-k]
+    return ''.join(stack)
 
         
-            
-            
     
-                
+
+
+
+# from itertools import combinations
+# def solution(number, k):
+#     ls_1 = []
+#     ls_2 = []
+#     for i in number:
+#         ls_1.append(i)
+#     for p in combinations(ls_1, len(number)-k):
+#         if p[0] == max(ls_1):
+#             ls_2.append(''.join(p))
+#     ls_2.sort()
+        
+#     return max(ls_2)
