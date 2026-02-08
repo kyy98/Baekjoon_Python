bed418ec76fa3414a187bf30410c9ce5ccae5188
YooBao <164113940+kyy98@users.noreply.github.com>
2026-02-08 18:32:21 +0900

    [Silver I] Title: 단지번호붙이기, Time: 36 ms, Memory: 32472 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,43 +1,34 @@
-from collections import deque
+def dfs(x,y):
+    if x<=-1 or x>=n or y<=-1 or y>=n:
+        return False
+    if graph[x][y] == 1:
+        graph[x][y] = 0
+        global ans
+        ans+=1
 
-dx = [-1,1,0,0]
-dy = [0,0,-1,1]
-ls = []
+        dfs(x-1,y)
+        dfs(x+1,y)
+        dfs(x,y-1)
+        dfs(x,y+1)
 
-def bfs(x,y):
-    q = deque([])
-    q.append((x,y))
-    graph[x][y] = 0
-    cnt_2 = 1
-
-    while q:
-        x,y = q.popleft()
-
-        for i in range(4):
-            nx = x+dx[i]
-            ny = y+dy[i]
-
-            if nx<=-1 or nx>=n or ny<=-1 or ny>=n:
-                continue
-            if graph[nx][ny] == 1:
-                graph[nx][ny] = 0
-                q.append((nx,ny))
-                cnt_2+=1
-    ls.append(cnt_2)
+        return True
+    return False
 
 n = int(input())
 graph = []
-
 for _ in range(n):
     graph.append(list(map(int, input())))
 
-cnt_1 = 0
+cnt = 0
+ans = 0
+ls = []
 for i in range(n):
     for j in range(n):
-        if graph[i][j] == 1:
-            bfs(i,j)
-            cnt_1+=1
+        if dfs(i,j) == True:
+            cnt+=1
+            ls.append(ans)
+            ans = 0
 
-print(cnt_1, end='\n')
 ls.sort()
+print(cnt, end='\n')
 print(*ls, sep='\n')
