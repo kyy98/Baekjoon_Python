8866f097ee6e8123aa5c997030c3afbbe02843dc
YooBao <164113940+kyy98@users.noreply.github.com>
2026-03-09 12:45:31 +0900

    [Silver II] Title: 유기농 배추, Time: 220 ms, Memory: 34984 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,33 +1,38 @@
-import sys
-# 재귀 깊이 제한 설정 (필수)
-sys.setrecursionlimit(10000)
+from collections import deque
 
-def dfs(x,y):
-    if x<0 or x>=m or y<0 or y>=n:
-        return False
-  
-    if graph[y][x] == 1:
-        graph[y][x] = 0
+dx = [-1,1,0,0]
+dy = [0,0,-1,1]
 
-        dfs(x-1,y)
-        dfs(x+1,y)
-        dfs(x,y-1)
-        dfs(x,y+1)
+def bfs(x,y):
+    q = deque([])
+    q.append((x,y))
+    graph[x][y] = 0
 
-        return True
-    return False
+    while q:
+        x,y = q.popleft()
+        for i in range(4):
+            nx = x+dx[i]
+            ny = y+dy[i]
+
+            if nx<0 or nx>=n or ny<0 or ny>=m:
+                continue
+            if graph[nx][ny] == 1:
+                q.append((nx,ny))
+                graph[nx][ny] = 0
 
 t = int(input())
-for _ in range(t):
-    m, n, k = map(int, input().split())
-    graph = [[0]*m for p in range(n)]
-    for i in range(k):
-        a, b = map(int, input().split())
+
+for c in range(t):
+    m,n,k = map(int, input().split())
+    graph = [[0]*m for _ in range(n)]
+    for _ in range(k):
+        a,b = map(int, input().split())
         graph[b][a] = 1
 
     cnt = 0
-    for i in range(m):
-        for j in range(n):
-            if dfs(i, j) == True:
+    for i in range(n):
+        for j in range(m):
+            if graph[i][j] == 1:
+                bfs(i,j)
                 cnt+=1
     print(cnt)
