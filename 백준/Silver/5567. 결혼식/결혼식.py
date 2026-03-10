f376c179e569208619eb77af2511137ccce5daf9
YooBao <164113940+kyy98@users.noreply.github.com>
2026-03-10 12:16:52 +0900

    [Silver II] Title: 결혼식, Time: 312 ms, Memory: 34944 KB -BaekjoonHub

--- 백준/Silver/5567. 결혼식/결혼식.py
+++ 백준/Silver/5567. 결혼식/결혼식.py
@@ -1,22 +1,23 @@
 from collections import deque
+n = int(input())
+m = int(input())
 
 def bfs(start, graph, visited):
     q = deque([start])
-    visited[start] = 1
+    visited[start] = 0
     while q:
         v = q.popleft()
         for i in graph[v]:
-            if visited[i] == 0:
+            if visited[i] == -1:
                 visited[i] = visited[v]+1
                 q.append(i)
-    return visited.count(2)+visited.count(3)
-n = int(input())
-m = int(input())
+    return visited.count(1)+visited.count(2)
+
+
 graph = [[] for _ in range(n+1)]
-visited=[0]*(n+1)
+visited = [-1]*(n+1)
 for _ in range(m):
-    a,b = map(int, input().split())
+    a,b = map(int,input().split())
     graph[a].append(b)
     graph[b].append(a)
-
 print(bfs(1,graph,visited))
