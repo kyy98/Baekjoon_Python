8c7c8ff4853c18510a7f7c1ccaa02ffbb9487513
YooBao <164113940+kyy98@users.noreply.github.com>
2026-03-10 10:04:56 +0900

    [Silver I] Title: 나이트의 이동, Time: 2204 ms, Memory: 34976 KB -BaekjoonHub

--- 백준/Silver/7562. 나이트의 이동/나이트의 이동.py
+++ 백준/Silver/7562. 나이트의 이동/나이트의 이동.py
@@ -1,32 +1,28 @@
 from collections import deque
+array = [(2,1),(2,-1),(-2,1),(-2,-1),(1,2),(1,-2),(-1,2),(-1,-2)]
 
-def bfs(x,y,z,w):
+def bfs(sx,sy,ex,ey):
     q = deque([])
-    q.append((x,y))
-    graph[x][y] = 1
-
+    q.append((sx,sy))
+    graph[sx][sy] = 1
+  
     while q:
         x,y = q.popleft()
-        for p in range(8):
-            nx = x+dx[p]
-            ny = y+dy[p]
-            if nx<0 or nx>=i or ny<0 or ny>=i:
+        for i in range(len(array)):
+            dx = x+array[i][0]
+            dy = y+array[i][1]
+
+            if dx<0 or dx>=l or dy<0 or dy>=l:
                 continue
-            if graph[nx][ny] == 0:
-                graph[nx][ny] = graph[x][y]+1
-                q.append((nx,ny))
-                if (nx,ny) == (z,w):
-                    break
-    return graph[z][w]-1
+            if graph[dx][dy] == 0:
+                graph[dx][dy] = graph[x][y]+1
+                q.append((dx,dy))
+    return graph[ex][ey]-1
 
 t = int(input())
 for _ in range(t):
-    i = int(input())
-    graph = [[0]*i for k in range(i)]
-    ci, cj = map(int, input().split())
-    ei, ej = map(int, input().split())
-
-    dx = [-2,-1,1,2,2,1,-1,-2]
-    dy = [-1,-2,-2,-1,1,2,2,1]
-
-    print(bfs(ci,cj,ei,ej))
+    l = int(input())
+    s_x,s_y = map(int, input().split())
+    e_x,e_y = map(int, input().split())
+    graph = [[0]*l for _ in range(l)]
+    print(bfs(s_x,s_y,e_x,e_y))
