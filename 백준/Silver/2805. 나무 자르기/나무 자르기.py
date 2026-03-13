b4edd3394c202df7bf610251cabc87b036312ce9
YooBao <164113940+kyy98@users.noreply.github.com>
2026-03-13 15:42:30 +0900

    [Silver II] Title: 나무 자르기, Time: 2272 ms, Memory: 145580 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.py
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -1,21 +1,21 @@
-import sys
+n,m = map(int, input().split())
+array = list(map(int, input().split()))
 
-n, m = map(int, sys.stdin.readline().split())
-array = list(map(int, sys.stdin.readline().split()))
+def binary_search(start, end, target):
+    res = 0
+    while start <= end:
+        result = 0
+        mid = (start+end)//2
+        for x in array:
+            if x > mid:
+                result+=x-mid
+        if result < target:
+            end = mid-1
+        else:
+            res = mid
+            start = mid+1
+    return res
 
-start = 0
-end = max(array)
+print(binary_search(0,max(array),m))
 
-while start<=end:
-    total = 0
-    mid = (start+end)//2
-    for x in array:
-        if x>mid:
-            total+=x-mid
-    if total < m:
-        end = mid-1
-    else:
-        result = mid
-        start = mid+1
 
-print(result)
