cb10c63968b639343e4dcc1e7012aa3272e1c0d8
YooBao <164113940+kyy98@users.noreply.github.com>
2026-04-16 14:20:06 +0900

    [level 2] Title: 타겟 넘버, Time: 601.79 ms, Memory: 99.1 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -2,34 +2,14 @@ from collections import deque
 
 def solution(numbers, target):
     q = deque([(0,0)])
-    cnt = 0
+    answer = 0
+    
     while q:
-        idx, res = q.popleft()        
+        idx, res = q.popleft()
         if idx == len(numbers):
             if res == target:
-                cnt+=1
+                answer+=1
         else:
             q.append((idx+1, res+numbers[idx]))
-            q.append((idx+1, res-numbers[idx]))
-    return cnt
-            
-    
-
-# def solution(nums, target):
-#     leaves = [0] # 모든 계산 결과를 담음
-#     cnt = 0
-    
-#     for num in nums:
-#         temp = []
-        
-#         for leaf in leaves:
-#             temp.append(leaf + num)
-#             temp.append(leaf - num)
-       
-#         leaves = temp
-        
-#     for leaf in leaves:
-#         if leaf == target:
-#             cnt += 1
-    
-#     return cnt
+            q.append((idx+1,res-numbers[idx]))
+    return answer
