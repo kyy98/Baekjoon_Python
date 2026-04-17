ef144bc8f875ecc94370077a47ae570e20f18794
YooBao <164113940+kyy98@users.noreply.github.com>
2026-04-17 15:41:50 +0900

    [level 2] Title: 의상, Time: 0.01 ms, Memory: 9.29 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,13 +1,16 @@
 def solution(clothes):
-    dict = {}
-    num = 1
+    dic = {}
     for c in clothes:
-        if c[1] not in dict:
-            dict[c[1]] = [c[0]]
+        if c[1] not in dic:
+            dic[c[1]] = [c[0]]
         else:
-            dict[c[1]] += [c[0]]
-    for v in dict.values():
-        num *= len(v)+1
-    return num-1
+            dic[c[1]].append(c[0])
+    cnt = 1
+    for v in dic.values():
+        cnt*=len(v)+1
+    return cnt-1
+        
+            
+    
         
     
