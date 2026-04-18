afc5602b8bdd75a5198ec1787a24a341291fce62
YooBao <164113940+kyy98@users.noreply.github.com>
2026-04-18 18:00:08 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 143.39 ms, Memory: 27.1 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -1,14 +1,14 @@
-
+from collections import deque
 def solution(arr):
-    ls = [arr[0]]
+    ans = [arr[0]]
     for i in range(1, len(arr)):
-        if ls[-1] == arr[i]:
-            ls.pop()
-            ls.append(arr[i])
+        if ans[-1] != arr[i]:
+            ans.append(arr[i])
         else:
-            ls.append(arr[i])
-    return ls
-   
+            ans.pop()
+            ans.append(arr[i])
+    return ans
+        
 
 
         
