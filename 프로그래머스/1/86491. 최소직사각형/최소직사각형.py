ee0c8cdd7c77dbf1d0e3762911d70677f5af229e
YooBao <164113940+kyy98@users.noreply.github.com>
2026-04-19 16:31:45 +0900

    [level 1] Title: 최소직사각형, Time: 3.08 ms, Memory: 10.8 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
@@ -1,41 +1,16 @@
 def solution(sizes):
-    w = []
-    h = []
-    
     for i in range(len(sizes)):
-      
-        if sizes[i][0] > sizes[i][1]:
-            w.append(sizes[i][0])
-            h.append(sizes[i][1])
-        else:
-            w.append(sizes[i][1])
-            h.append(sizes[i][0])
-    return max(w)*max(h)
-#     ls_1 = []
-#     ls_2 = []
-#     for i in sizes:
-#         ls_1.append(i[0])
-#         ls_2.append(i[1])
-        
-#     if max(ls_1) > max(ls_2):
-#         for s in sizes:
-#             if s[0] < s[1]:
-#                 s[0], s[1] = s[1], s[0]
-#         ls_3 = []
-#         for i in sizes:
-#             ls_3.append(i[1])
-#         return max(ls_1)*max(ls_3)
-#     else:
-#         for s in sizes:
-#             if s[0] > s[1]:
-#                 s[0], s[1] = s[1], s[0]
-#         ls_4 = []
-#         for i in sizes:
-#             ls_4.append(i[0])
-#         return max(ls_4)*max(ls_2)
-#     return max(ls_1)*max(ls_2)
-        
-        
+        if sizes[i][0]<sizes[i][1]:
+            tmp = sizes[i][1]
+            sizes[i][1] =sizes[i][0]
+            sizes[i][0] = tmp
+    ls_1 = []
+    ls_2 = []
+    
+    for s in sizes:
+        ls_1.append(s[0])
+        ls_2.append(s[1])
+    return max(ls_1)*max(ls_2)
 
         
             
